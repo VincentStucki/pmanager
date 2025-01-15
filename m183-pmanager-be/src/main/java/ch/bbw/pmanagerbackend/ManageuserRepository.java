a30116728086de433ffa63f4b22cd625a49aa6c2
Vincent Stucki <130548515+VincentStucki@users.noreply.github.com>
2025-01-15 15:41:39 +0100

    Delete m183-pmanager-be directory

--- m183-pmanager-be/src/main/java/ch/bbw/pmanagerbackend/ManageuserRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.bbw.pmanagerbackend;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManageuserRepository extends CrudRepository<Manageuser, Integer> {
-}
--- m183-pmanager-be/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-
-
-INSERT INTO manageuser (website, username, password, remarks) VALUES
-('www.whatever.com', 'm.muster@muster.com', 'wow123', 'hallo hallo');
-
-
-
